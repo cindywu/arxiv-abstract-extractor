caa93010d53413cc159e670d0a6cf446df860698
cindy <ciindy.wu@gmail.com>
2021-04-10 08:29:07 -1000

    get arxiv paper abstract

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
+import { parseString } from 'xml2js'
 
 function App() {
+  const getAbstract = () => {
+    console.log('gotten abstract')
+    axios.get(`http://export.arxiv.org/api/query?search_query=all:electron&start=0&max_results=1`)
+      .then(res => {
+        let returned = res.data
+        parseString(returned, function (err, result) {
+          console.dir(result.feed.entry[0].summary[0]);
+        })
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>arxiv abstract getter</h1>
+      <textarea placeholder="abstract goes here"></textarea>
+      <br></br>
+      <button onClick={getAbstract}>
+        get abstract
+      </button>
     </div>
-  );
+  )
 }
 
 export default App;
